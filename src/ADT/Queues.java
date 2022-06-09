6689a45b74c45a7c42835d6c8aee59a61c47a6c0
smartiri <rasa7825@gmail.com>
2022-06-09 18:24:58 +0200

    agan

---
+++ src/ADT/Queues.java
@@ -0,0 +1,8 @@
+package ADT;
+
+public interface Queues <T>{
+    void enqueue(T value);
+    T dequeue();
+    // sara - he per he vetem kto :P
+}
+
---
+++ src/Controller.java
@@ -0,0 +1,4 @@
+public class Controller {
+    // ketu do jene ato metodat statike per te therritur si psh funksioni me krijuar node ne frontend
+    // ose dhe ndonje gje tjeter. 
+}
