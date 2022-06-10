6f0914cec4c5b0189de1771bbd1f1b638e8120a1
TerensTare <terens.t17@gmail.com>
2022-06-10 20:23:23 +0200

    Move widgets handling to separate class.

--- src/App.java
+++ src/App.java
@@ -1,80 +1,44 @@
-import java.util.ArrayList;
-
-import javax.swing.Action;
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        stage.getIcons().add(new Image("/images/icon2.jpg"));
-        stage.setTitle("QUEUES");
-        TextField txt = new TextField();
-        Button enqueue = new Button();
-        enqueue.setText("Enqueue");
-        Button dequeue = new Button();
-        dequeue.setText("Dequeue");
-        Button find = new Button();
-        find.setText("Find");
+        WidgetManager widgets = new WidgetManager();
+
+        TextField txt = widgets.textField();
+
+        Button enqueue = widgets.enqueue();
+        Button dequeue = widgets.dequeue();
+        Button find = widgets.find();
+
         HBox inputBox = new HBox();
         inputBox.setPadding(new Insets(10));
         inputBox.setSpacing(10);
         inputBox.getChildren().addAll(txt, enqueue, dequeue, find);
-        BorderPane bp = new BorderPane();
-        VBox root = new VBox(inputBox);
+
+        VBox root = new VBox(inputBox, widgets.queue());
         root.setAlignment(Pos.CENTER);
+
+        BorderPane bp = new BorderPane();
         bp.setTop(root);
-        HBox queue = new HBox();
-        queue.setSpacing(20);
-        ArrayList<TextField> list = new ArrayList<>(); // list qe ruan cdo txt
-        enqueue.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                StackPane st = new StackPane();
-                st = createNode(txt);
-                queue.getChildren().add(st);
-                list.add(txt); // shton txt n list q t rujm cdo input, mund t perdoret pr funksione t tjera tn
-                               // e km ber pr dequeue
-            }
-        });
-        dequeue.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent arg0) {
-                StackPane st = new StackPane();
-                st = createNode(list.get(0)); // krijon n st me index 0 t listes se txt
-                int index = queue.getChildren().indexOf(st) + 1; // mer index te st tek children t queue
-                queue.getChildren().remove(index); // fshin child n index, q do jet i pari
-            }
-        });
-        root.getChildren().add(queue);
+
+        stage.getIcons().add(new Image("/images/icon2.jpg"));
+        stage.setTitle("QUEUES");
         stage.setScene(new Scene(bp, 1200, 600));
         stage.show();
     }
 
     public static void main(String[] args) throws Exception {
         launch(args);
-    }
-
-    private static <T> StackPane createNode(TextField value) {
-        StackPane pane = new StackPane(
-                new Rectangle(200, 100, Color.AQUAMARINE),
-                new Label(value.getText()));
-        return pane;
     }
 }
---
+++ src/WidgetManager.java
@@ -0,0 +1,120 @@
+import java.util.ArrayList;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class WidgetManager {
+    private ArrayList<TextField> list;
+
+    private HBox queue; // queue frontend
+    private TextField txt; // duhet ta ruajme ketu sepse aksesohen nga butonat
+    // butonat
+    private Button enqueue;
+    private Button dequeue;
+    private Button find;
+
+    public WidgetManager() {
+        list = new ArrayList<>();
+
+        queue = new HBox();
+        queue.setSpacing(20);
+
+        txt = new TextField();
+        txt.setPromptText("Enter a value");
+
+        setupEnqueueButton();
+        setupDequeueButton();
+        setupFindButton();
+
+        // fillimisht butonat qe duan nje queue jo boshe nuk shfaqen
+        showButtons(false);
+    }
+
+    public HBox queue() {
+        return queue;
+    }
+
+    public TextField textField() {
+        return txt;
+    }
+
+    public Button enqueue() {
+        return enqueue;
+    }
+
+    Button dequeue() {
+        return dequeue;
+    }
+
+    Button find() {
+        return find;
+    }
+
+    // metodat private
+
+    private void setupEnqueueButton() {
+        enqueue = new Button();
+        enqueue.setText("Enqueue");
+
+        enqueue.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                StackPane st = createNode(txt);
+                queue.getChildren().add(st);
+                list.add(txt); // shton txt n list q t rujm cdo input, mund t perdoret pr funksione t tjera tn
+                               // e km ber pr dequeue
+                txt.clear();
+
+                // shfaqi butonat e fshehur
+                showButtons(true);
+            }
+        });
+    }
+
+    private void setupDequeueButton() {
+        dequeue = new Button();
+        dequeue.setText("Dequeue");
+
+        dequeue.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent arg0) {
+                StackPane st = new StackPane();
+                st = createNode(list.get(0)); // krijon n st me index 0 t listes se txt
+                int index = queue.getChildren().indexOf(st) + 1; // mer index te st tek children t queue
+                queue.getChildren().remove(index); // fshin child n index, q do jet i pari
+
+                // fshihi butonat nqs queue eshte bosh
+                if (queue.getChildren().size() == 0) {
+                    showButtons(false);
+                }
+            }
+        });
+    }
+
+    private void setupFindButton() {
+        find = new Button();
+        find.setText("Find");
+
+        // TODO: handle click event
+    }
+
+    private void showButtons(boolean show) {
+        dequeue.setVisible(show);
+        find.setVisible(show);
+    }
+
+    private static <T> StackPane createNode(TextField value) {
+        StackPane pane = new StackPane(
+                new Rectangle(200, 100, Color.AQUAMARINE),
+                new Label(value.getText()) //
+        );
+        return pane;
+    }
+}
