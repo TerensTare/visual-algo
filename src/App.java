1213cc0957b002461e9f07bfb89843db175ce0b9
Elisa_H <elisahaxhillazi@gmail.com>
2022-06-11 12:28:57 +0200

    GridPane to keep all buttons

--- src/App.java
+++ src/App.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -13,19 +14,25 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+       
         WidgetManager widgets = new WidgetManager();
-
         TextField txt = widgets.textField();
-
         Button enqueue = widgets.enqueue();
         Button dequeue = widgets.dequeue();
         Button clear = widgets.clear();
         Button find = widgets.find();
 
         HBox inputBox = new HBox();
+        GridPane buttonPane = new GridPane();
+        buttonPane.add(enqueue, 1,1);
+        buttonPane.add(dequeue, 2,1);
+        buttonPane.add(clear, 3,1);
+        buttonPane.add(find, 4,1);
+        buttonPane.setHgap(10);
+
         inputBox.setPadding(new Insets(10));
-        inputBox.setSpacing(10);
-        inputBox.getChildren().addAll(txt, enqueue, dequeue, clear, find);
+        inputBox.setSpacing(60);
+        inputBox.getChildren().addAll(txt, buttonPane);
 
         VBox root = new VBox(inputBox, widgets.queue());
         root.setAlignment(Pos.CENTER);
