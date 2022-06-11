dcf29922f683b24a368f38b2744b15522dd4521e
Elisa_H <elisahaxhillazi@gmail.com>
2022-06-11 17:21:08 +0200

    Background color gradient

--- src/App.java
+++ src/App.java
@@ -9,12 +9,26 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.RadialGradient;
+import javafx.scene.paint.Stop;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-       
+        
+        BorderPane bp = new BorderPane();
+        bp.setStyle("-fx-background-color: transparent;");
+        Scene scene = new Scene(bp, 1200, 600);
+        scene.setFill(new RadialGradient(
+        0, 0, 0, 0, 1, true,                  
+        CycleMethod.NO_CYCLE,                 
+        new Stop(0, Color.web("#81c483")),    
+        new Stop(1, Color.web("#fcc200")))
+);
+
         WidgetManager widgets = new WidgetManager();
         TextField txt = widgets.textField();
         Button enqueue = widgets.enqueue();
@@ -29,6 +43,7 @@ public class App extends Application {
         buttonPane.add(clear, 3,1);
         buttonPane.add(find, 4,1);
         buttonPane.setHgap(10);
+        inputBox.setStyle("-fx-background-color: transparent;");
 
         inputBox.setPadding(new Insets(0, 0, 20, 0));
         inputBox.setSpacing(60);
@@ -37,13 +52,16 @@ public class App extends Application {
         VBox root = new VBox(inputBox, widgets.queue());
         root.setAlignment(Pos.CENTER);
         root.setPadding(new Insets(10, 10, 10, 10));
+        root.setStyle("-fx-background-color: transparent;");
 
-        BorderPane bp = new BorderPane();
+       
         bp.setTop(root);
+        bp.setStyle("-fx-background-color: transparent;");
+
 
         stage.getIcons().add(new Image("/images/icon2.jpg"));
         stage.setTitle("QUEUES");
-        stage.setScene(new Scene(bp, 1200, 600));
+        stage.setScene(scene);
         stage.show();
     }
 
